1a76d55bd53d63cedc61a49bd147c2801169ce26
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-10-12 10:29:51 +0530

    updated User model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -46,4 +46,5 @@ userSchema.methods.toJSON = function () {
 };
 
 const User = mongoose.model('User', userSchema);
-export default User;
+
+module.exports = User;
